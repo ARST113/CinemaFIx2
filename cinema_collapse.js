d894f44d44eed4e7d507da7353fe2de88dbf9964
ARST113 <96774211+ARST113@users.noreply.github.com>
2025-03-30 20:00:46 +0300

    Update cinema_collapse.js

--- cinema_collapse.js
+++ cinema_collapse.js
@@ -4,9 +4,9 @@
     // Функция для логирования всех источников (для отладки)
     function logSources() {
         const sources = Lampa.Activity.sources || [];
-        console.log('[CinemaCollapseFix] Список источников:');
+        console.log('[CinemaCollapseFix] Список источников:', sources.length);
         sources.forEach((src, index) => {
-            console.log(index, src.name);
+            console.log(index, src ? src.name : 'нет поля name');
         });
     }
 
@@ -19,19 +19,22 @@
 
             for (let i = 0; i < sources.length; i++) {
                 const src = sources[i];
-                // Если имя источника содержит "cinema" (любым регистром)
-                // и collapse еще не определен, то добавляем его
-                if (src.name && src.name.toLowerCase().indexOf('cinema') !== -1 && typeof src.collapse !== 'function') {
-                    src.collapse = function () {
-                        return {
-                            title: 'Cinema',
-                            description: 'Источник от Cinema',
-                            sort: 1
+                // Проверяем, что src существует и src.name - строка
+                if (src && typeof src.name === 'string') {
+                    const lowerName = src.name.toLowerCase();
+                    // Если имя источника содержит "cinema"
+                    if (lowerName.includes('cinema') && typeof src.collapse !== 'function') {
+                        src.collapse = function () {
+                            return {
+                                title: 'Cinema',
+                                description: 'Источник от Cinema',
+                                sort: 1
+                            };
                         };
-                    };
-                    console.log('[CinemaCollapseFix] collapse() добавлен для источника:', src.name);
-                    clearInterval(INTERVAL);
-                    break;
+                        console.log('[CinemaCollapseFix] collapse() добавлен для источника:', src.name);
+                        clearInterval(INTERVAL);
+                        break;
+                    }
                 }
             }
         }
