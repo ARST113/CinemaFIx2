3624b105ea0e16f2ac07245f864b5a50ebd0875a
ARST113 <96774211+ARST113@users.noreply.github.com>
2025-03-31 21:21:37 +0300

    Update cinema_collapse.js

--- cinema_collapse.js
+++ cinema_collapse.js
@@ -1,26 +1,74 @@
 (function () {
     'use strict';
 
-    console.log('[FixCinemaPosition] Загружен');
+    console.log('[CinemaWrapper] плагин загружен');
 
-    Lampa.Listener.follow('full', function (e) {
-        if (e.type === 'complite') {
-            setTimeout(() => {
-                try {
-                    const activity = e.object.activity.render();
-                    const cinemaButton = activity.find('.cinema--button');
-                    const target = activity.find('.full-start-new__buttons');
+    function waitForElement(selector, timeout = 5000) {
+        return new Promise((resolve) => {
+            const interval = 50;
+            let elapsed = 0;
 
-                    if (cinemaButton.length && target.length) {
-                        target.prepend(cinemaButton); // перемещаем первым
-                        console.log('[FixCinemaPosition] Кнопка перемещена внутрь .full-start-new__buttons');
-                    } else {
-                        console.warn('[FixCinemaPosition] Контейнер или кнопка не найдены');
-                    }
-                } catch (err) {
-                    console.error('[FixCinemaPosition] Ошибка при перемещении кнопки:', err);
+            const timer = setInterval(() => {
+                const el = document.querySelector(selector);
+                if (el) {
+                    clearInterval(timer);
+                    resolve(el);
+                } else if ((elapsed += interval) >= timeout) {
+                    clearInterval(timer);
+                    resolve(null);
                 }
-            }, 300); // дать Lampa время вставить кнопку
-        }
+            }, interval);
+        });
+    }
+
+    Lampa.Listener.follow('full', async function (e) {
+        if (e.type !== 'complite') return;
+
+        setTimeout(async () => {
+            const activity = Lampa.Activity.active();
+            const render = activity.render();
+
+            // Ждём кнопку Cinema
+            const btn = render.find('.view--online_cinema');
+            if (!btn.length) return;
+
+            console.log('[CinemaWrapper] кнопка Cinema найдена, скрываем из основного интерфейса');
+
+            // Удаляем кнопку Cinema из главного экрана
+            btn.remove();
+
+            // Добавляем новый источник в список "Смотреть"
+            Lampa.Player.addPlugin({
+                title: 'Cinema',
+                component: 'cinema_online',
+                type: 'video',
+                onContextMenu: function (object) {
+                    return {
+                        name: 'Cinema',
+                        description: 'Смотреть через плагин Cinema',
+                    };
+                },
+                onContextLauch: function (object) {
+                    // Используем те же действия, что и оригинальный Cinema
+                    var id = Lampa.Utils.hash(object.number_of_seasons ? object.original_name : object.original_title);
+                    var all = Lampa.Storage.get('clarification_search', '{}');
+
+                    Lampa.Component.add('cinema_online'); // обязательно, если компонент ещё не зарегистрирован
+                    Lampa.Activity.push({
+                        url: '',
+                        title: Lampa.Lang.translate('title_online'),
+                        component: 'cinema_online',
+                        search: all[id] ? all[id] : object.title,
+                        search_one: object.title,
+                        search_two: object.original_title,
+                        movie: object,
+                        page: 1,
+                        clarification: all[id] ? true : false,
+                    });
+                },
+            });
+
+            console.log('[CinemaWrapper] Cinema добавлена как источник');
+        }, 500); // Подстраховка: Cinema может добавляться с задержкой
     });
 })();
