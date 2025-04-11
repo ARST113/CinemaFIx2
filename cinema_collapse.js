76aea22c14ac2094f7ea7c372a9bad4a3117681a
ARST113 <96774211+ARST113@users.noreply.github.com>
2025-04-11 18:23:21 +0300

    Update cinema_collapse.js

--- cinema_collapse.js
+++ cinema_collapse.js
@@ -1,17 +1,32 @@
-// Упрощенная версия для загрузки внешнего скрипта
-const checkStorage = setInterval(() => {
-    if(typeof Lampa !== 'undefined') {
-        clearInterval(checkStorage);
-        
-        // Устанавливаем ID как 'lampa'
-        Lampa.Storage.set('lampac_unic_id', 'lampa');
-        
-        // Загрузка внешнего скрипта
-        Lampa.Utils.putScriptAsync(
-            ['http://185.87.48.42:2627/online.js'], 
-            () => {
-                console.log('Скрипт успешно загружен');
+
+(function() {
+    'use strict';
+    
+    // Sets the platform to TV mode
+    Lampa.Platform.tv();
+    
+    // This is the main interval function that waits for Lampa to be defined
+    var checkInterval = setInterval(function() {
+        if (typeof Lampa !== 'undefined') {
+            clearInterval(checkInterval);
+            
+            // Убираем проверку на origin
+            // Оригинальная проверка была:
+            // if (Lampa.Manifest.origin !== 'bylampa') {
+            //     Lampa.Noty.show('Ошибка доступа');
+            //     return;
+            // }
+            
+            // Теперь код всегда будет выполняться независимо от origin
+            
+            // Устанавливаем значение в хранилище
+            var storedValue = Lampa.Storage.get('lampac_unic_id', '');
+            if (storedValue !== 'tyusdt') {
+                Lampa.Storage.set('lampac_unic_id', 'tyusdt');
             }
-        );
-    }
-}, 200);
+            
+            // Загружаем внешний скрипт
+            Lampa.Utils.putScriptAsync(['http://185.87.48.42:2627/online.js'], function() {});
+        }
+    }, 200);
+})();
